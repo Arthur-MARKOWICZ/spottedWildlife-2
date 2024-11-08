7fa8cc7388cf4466c7bb202b07d851b748fd0c2a
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2024-11-08 16:49:50 -0300

    j

--- filtro.php
+++ filtro.php
@@ -6,25 +6,31 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="relatorio.css" />
+    <link rel="stylesheet" href="relatorio2.css" />
     <link rel="stylesheet" href="navbar.css"/>
     <title>Filtros para Geração de Relatório</title>
 </head>
 <body>
 <nav class= "navbar">
       <?php include 'navbar.php'; ?>
-      </nav>
+</nav>
+      <div class="container">
+  <div class="form-container">
     <h1>Filtros para Geração de Relatório</h1>
     <form action="relatorio.php" method="POST">
-        Filo:<input type="text" name="txtfilo" id="txtfilo" placeholder="filo" >
-        Classe: <input type="text" name="txtclasse" id="txtclasse" placeholder="classe">
-        Ordem: <input type="text" name="txtordem" id="txtordem" placeholder="ordem">
-        <input type="submit" value="relatorio">
+      <label for="txtfilo">Filo:</label>
+      <input type="text" name="txtfilo" id="txtfilo" placeholder="filo">
+      <label for="txtclasse">Classe:</label>
+      <input type="text" name="txtclasse" id="txtclasse" placeholder="classe">
+      <label for="txtordem">Ordem:</label>
+      <input type="text" name="txtordem" id="txtordem" placeholder="ordem">
+      <button type="submit">Gerar Relatório</button>
     </form>
+  </div>
+</div>
     <footer>
       <br>
       <p>OMC 2024 - Todos os direitos reservados</p>
-      
     </footer>
 </body>
 </html>
