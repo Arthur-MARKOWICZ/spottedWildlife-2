fc1982d93b24b5c568a95cccd856b3f5ed4d7136
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2024-11-08 18:06:29 -0300

    O

--- cadastraanimal.php
+++ cadastraanimal.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="animalcadastro.css"/>
+    <link rel="stylesheet" href="animalcadastro2.css"/>
     <link rel="stylesheet" href="navbar.css"/>
 </head>
 <body>
--- cadastroespecialista.php
+++ cadastroespecialista.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="animalcadastro.css"/>
+    <link rel="stylesheet" href="CADASTROESP.css"/>
     <link rel="stylesheet" href="navbar.css"/>
 </head>
 
