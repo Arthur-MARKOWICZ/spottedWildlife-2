f4405c465bb70d5a48ebe25c2b4ef9e273b9747c
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2024-11-09 09:52:52 -0300

    correcao de bugs

--- editardados.php
+++ editardados.php
@@ -9,7 +9,7 @@
         $nome = $usuariodados['nome'];
         $cpf = $usuariodados['cpf'];
         $email = $usuariodados['email_pessoal'];
-        $senha = $usuariodados['senha_user'];
+        $senha = $_SESSION['senha'];
         $dataNasc = $usuariodados['data_nasc'];
         $telefone = $usuariodados['telefone'];
         $pais = $usuariodados['end_pais'];
@@ -18,21 +18,27 @@
         $nome_user = $usuariodados['nome_user'];
         $cidade = $usuariodados['end_cidade'];
         $sexo = $usuariodados['sexo'];
+    
         
     }
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="cadastro.css" />
+    <link rel="stylesheet" href="editardados.css" />
+    <link rel="stylesheet" href="navbar.css" />
     <title>editar dados do cadastro</title>
   </head>
   <br>
     <header>
       <img src="" alt="" />
     </header>
+    <nav class= "navbar">
+          <?php include 'navbar.php'; ?>
+      </nav>
     <h1>Editar dados do cadastro</h1>
   <div vw class="enabled">
     <div vw-access-button class="active"></div>
@@ -44,73 +50,74 @@
   <script>
     new window.VLibras.Widget('https://vlibras.gov.br/app');
   </script>
-    <div class="conteiner">
-    <nav class= "navbar">
-      <?php include 'navbar.php'; ?>
-      </nav>
+
       
+
+
       <div class="conteiner">
 
       
       <form
         name="form_cadastro"
         method="POST"
-        action="cadastro.php"
+        action="salvarEditar.php"
         id="form_cadastro"
         onsubmit="return btnSendOnClick();"
         enctype="multipart/form-data"
       >
-        Nome: <input type="text" name="txtName" id="txtName" placeholder="Insira seu nome" /><br />
-        CPF <input type="text" name="txtCPF" id="txtCPF" placeholder="Insira seu CPF (Apenas números)" /><br />
-        Data de nascimento <input type="text" name="txtNasc" id="txtNasc" placeholder="(AAAA-MM-DD)"><br />
+        Nome: <input type="text" name="txtName" id="txtName" placeholder="Insira seu nome" value ="<?php echo "$nome" ?>" /><br />
+        CPF <input type="text" name="txtCPF" id="txtCPF" placeholder="Insira seu CPF (Apenas números) "value ="<?php echo "$cpf" ?> "/><br />
+        Data de nascimento <input type="text" name="txtNasc" id="txtNasc" placeholder="(AAAA-MM-DD)" value ="<?php echo "$dataNasc" ?>"><br />
         Sexo:
         <div style="display: flex; gap: 10px;">
           <label>
-            <input type="radio" name="optGender" value="feminino" id="feminino"/> Feminino
-          </label>
-          <label>
-            <input type="radio" name="optGender" value="masculino" id="masculino" /> Masculino
+          <input type="radio" name="optGender" value="feminino" id="feminino" <?php if ($sexo == 'feminino') echo 'checked'; ?> /> Feminino
+          <input type="radio" name="optGender" value="masculino" id="masculino" <?php if ($sexo == 'masculino') echo 'checked'; ?> /> Masculino
+
           </label>
         </div><br />
-        E-mail <input type="text" name="txtEmail" id="txtEmail" placeholder="Insira seu e-mail"/><br />
-        País <input type="text" name="end_pais" id="end_pais" placeholder="Insira o país que você mora"/><br />
+        E-mail <input type="text" name="txtEmail" id="txtEmail" placeholder="Insira seu e-mail"value ="<?php echo "$email" ?> "/><br />
+        País <input type="text" name="end_pais" id="end_pais" placeholder="Insira o país que você mora"value ="<?php echo "$pais" ?> "/><br />
         <label for="estado">Estado</label>
         <select id="end_estado" name="end_estado" required>
-          <option value="0" disabled selected>Selecione o estado:</option>
-          <option value="ac">AC</option>
-          <option value="al">AL</option>
-          <option value="am">AM</option>
-          <option value="ap">AP</option>
-          <option value="ba">BA</option>
-          <option value="ce">CE</option>
-          <option value="df">DF</option>
-          <option value="es">ES</option>
-          <option value="go">GO</option>
-          <option value="ma">MA</option>
-          <option value="mg">MG</option>
-          <option value="ms">MS</option>
-          <option value="mt">MT</option>
-          <option value="pa">PA</option>
-          <option value="pb">PB</option>
-          <option value="pe">PE</option>
-          <option value="pi">PI</option>
-          <option value="pr">PR</option>
-          <option value="rj">RJ</option>
-          <option value="rn">RN</option>
-          <option value="ro">RO</option>
-          <option value="rr">RR</option>
-          <option value="rs">RS</option>
-          <option value="sc">SC</option>
-          <option value="se">SE</option>
-          <option value="sp">SP</option>
-          <option value="to">TO</option></select
-        ><br />
-        Cidade <input type="text" name="end_cidade" id="end_cidade" placeholder="Insira a cidade que você mora" /><br />
-        Telefone <input type="text" name="txtTel" id="txtTel" placeholder="Insira seu telefone (Apenas números)" /><br />
-        Animal Favorito <input type="text" name="txtAF" id="txtAF" /><br />
-        Nome de usuário <input type="text" name="txtNU" id="txtNU" placeholder="Insira o nome que será exibido para os outros usuários" /><br />
-        Senha <input type="text" name="txtSenha" id="txtSenha" placeholder="Insira sua senha" /><br />
-        Confirmar sua senha <input type="text" placeholder="Confirme sua senha">
+            <option value="0" disabled <?php if (!$estado) echo 'selected'; ?>>Selecione o estado:</option>
+            <option value="ac" <?php if ($estado == 'ac') echo 'selected'; ?>>AC</option>
+            <option value="al" <?php if ($estado == 'al') echo 'selected'; ?>>AL</option>
+            <option value="am" <?php if ($estado == 'am') echo 'selected'; ?>>AM</option>
+            <option value="ap" <?php if ($estado == 'ap') echo 'selected'; ?>>AP</option>
+            <option value="ba" <?php if ($estado == 'ba') echo 'selected'; ?>>BA</option>
+            <option value="ce" <?php if ($estado == 'ce') echo 'selected'; ?>>CE</option>
+            <option value="df" <?php if ($estado == 'df') echo 'selected'; ?>>DF</option>
+            <option value="es" <?php if ($estado == 'es') echo 'selected'; ?>>ES</option>
+            <option value="go" <?php if ($estado == 'go') echo 'selected'; ?>>GO</option>
+            <option value="ma" <?php if ($estado == 'ma') echo 'selected'; ?>>MA</option>
+            <option value="mg" <?php if ($estado == 'mg') echo 'selected'; ?>>MG</option>
+            <option value="ms" <?php if ($estado == 'ms') echo 'selected'; ?>>MS</option>
+            <option value="mt" <?php if ($estado == 'mt') echo 'selected'; ?>>MT</option>
+            <option value="pa" <?php if ($estado == 'pa') echo 'selected'; ?>>PA</option>
+            <option value="pb" <?php if ($estado == 'pb') echo 'selected'; ?>>PB</option>
+            <option value="pe" <?php if ($estado == 'pe') echo 'selected'; ?>>PE</option>
+            <option value="pi" <?php if ($estado == 'pi') echo 'selected'; ?>>PI</option>
+            <option value="pr" <?php if ($estado == 'pr') echo 'selected'; ?>>PR</option>
+            <option value="rj" <?php if ($estado == 'rj') echo 'selected'; ?>>RJ</option>
+            <option value="rn" <?php if ($estado == 'rn') echo 'selected'; ?>>RN</option>
+            <option value="ro" <?php if ($estado == 'ro') echo 'selected'; ?>>RO</option>
+            <option value="rr" <?php if ($estado == 'rr') echo 'selected'; ?>>RR</option>
+            <option value="rs" <?php if ($estado == 'rs') echo 'selected'; ?>>RS</option>
+            <option value="sc" <?php if ($estado == 'sc') echo 'selected'; ?>>SC</option>
+            <option value="se" <?php if ($estado == 'se') echo 'selected'; ?>>SE</option>
+            <option value="sp" <?php if ($estado == 'sp') echo 'selected'; ?>>SP</option>
+            <option value="to" <?php if ($estado == 'to') echo 'selected'; ?>>TO</option>
+        </select>
+        <br />
+        Cidade <input type="text" name="end_cidade" id="end_cidade" placeholder="Insira a cidade que você mora"value ="<?php echo "$cidade" ?> " /><br />
+        Telefone <input type="text" name="txtTel" id="txtTel" placeholder="Insira seu telefone (Apenas números)"value ="<?php echo "$telefone" ?> " /><br />
+        Animal Favorito <input type="text" name="txtAF" id="txtAF" value ="<?php echo "$animalFav" ?> "/><br />
+        Nome de usuário <input type="text" name="txtNU" id="txtNU" placeholder="Insira o nome que será exibido para os outros usuários" value ="<?php echo "$nome_user" ?> "/><br />
+        Senha <input type="text" name="txtSenha" id="txtSenha" placeholder="Insira sua senha" value="<?php echo $senha; ?>"
+        />
+        <br />
+        Confirmar sua senha <input type="text" placeholder="Confirme sua senha" >
         <input type="submit" value="Enviar" />
         <input type="reset" value="Limpar" />
       </form>
@@ -118,7 +125,6 @@
   </body>
 
   <script>
-    //arumar o validacao(sem mexer no submit para nao ferrar o cadastro.php)
     const txtName = document.getElementById("txtName");
     const txtCPF = document.getElementById("txtCPF");
     const dataNasc = document.getElementById("txtNasc");
@@ -197,6 +203,12 @@
         txtEmail.focus();
         return false;
       }
+      if (!isDataNasc(dataNasc)) {
+        alert("Data de nascimento incorreta. Por favor, use o formato AAAA-MM-DD.");
+        dataNasc.focus();
+    return false;
+}
+
       return true;
     }
     function isCPF(txtcpf) {
@@ -206,11 +218,16 @@
       
     }
 
-    function isEmail(txtemail) {
-        const email =  txtemail.value;
-      const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    function isEmail(txtEmail) {
+      const email =  txtEmail.value.trim();
+      const re = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
       return re.test(email);
     }
-    // criar funcao regex para data de nacimento
+    function isDataNasc(txtNasc) {
+    const data = txtNasc.value;
+    const re = /^\d{4}-\d{2}-\d{2}$/; 
+    return re.test(data);
+}
+
   </script>
 </html>
--- editardadosanimal.php
+++ editardadosanimal.php
@@ -28,7 +28,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>Document</title>
-    <link rel="stylesheet" href="animalcadastro.css"/>
+    <link rel="stylesheet" href="editardadosanimal2.css"/>
+    <link rel="stylesheet" href="navbar.css"/>
 </head>
 <body>
 <div vw class="enabled">
