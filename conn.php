5d4eca573ff54e3e32d17cb77590c4c9cb108a70
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2024-11-14 14:38:08 -0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
     $dbhost = '193.203.175.140';
     $dbusuario = 'u989192386_root';
-    $senha = '@Spotted123';
+    $senhadb = '@Spotted123';
     $dbnome = 'u989192386_spotted';
-    $conn = new mysqli($dbhost,$dbusuario,$senha,$dbnome);
+    $conn = new mysqli($dbhost,$dbusuario,$senhadb,$dbnome);
 
 ?>
